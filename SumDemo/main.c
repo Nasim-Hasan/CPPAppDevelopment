0e9b6b5993de396ae9da85d7f63f1c10093b4216
Nasim-Hasan <nasimcse2k5@gmail.com>
2023-02-22 21:10:24 +0600

    Sum in C

---
+++ SumDemo/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int i,sum=0;
+
+    for(i=1;i<=10;i++)
+    {
+        sum=sum+i;
+    }
+
+    printf("Sum is:%d\n", sum);
+
+    return 0;
+}
